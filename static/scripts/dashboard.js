bb6f5a1862f8d8476a3f78578096964620ac953d
Lorenzo Pucci <lorenzo.pucci5@gmail.com>
2025-09-16 22:06:48 +0200

    Fix map tooltips (closes #2)

--- static/scripts/dashboard.js
+++ static/scripts/dashboard.js
@@ -10,15 +10,16 @@ function color_svg(data) {
     data.forEach((room) => {
         var path = document.getElementById(`path${room.floor}${room.svg_id}`);
         var text = document.getElementById(`text${room.floor}${room.svg_id}`);
+        var clickable = document.getElementById(`clickable${room.floor}${room.svg_id}`);
 
         path.style.fill = `rgb(${room.color[0]}, ${room.color[1]}, ${room.color[2]})`;
-        path.onclick = () => on_svg_click(room.id);
+        clickable.onclick = () => on_svg_click(room.id);
 
         var luminance = 0.2126 * room.color[0] + 0.7152 * room.color[1] + 0.0722 * room.color[2];
         text.style.fill = (luminance < 150) ? "white" : "black";
         text.textContent = room.label;
 
-        tippy(`#path${room.floor}${room.svg_id}`, {
+        tippy(`#clickable${room.floor}${room.svg_id}`, {
             content: room.tooltip,
             placement: 'top',
         })
